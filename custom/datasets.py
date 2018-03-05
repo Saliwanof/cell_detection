e38e4196d447b07b47f6f884826f1eda911033d6
Saliwanof <harrypotter_china@126.com>
2018-03-05 21:35:23 +0800

    add custom transforms

--- custom/datasets.py
+++ custom/datasets.py
@@ -1,3 +1,4 @@
+import torch
 import h5py
 import numpy as np
 from os.path import isdir
@@ -5,7 +6,7 @@ from pathlib import Path
 from PIL import Image
 import torchvision.transforms as transforms
 from scipy.ndimage.morphology import distance_transform_edt as dte 
-from .utils import conditioned_inverse, random_crop
+from .utils import to_brightfield, random_crop_256, random_flip, random_rotate
 
 
 class nuclei_dataset(torch.utils.data.Dataset):
@@ -13,18 +14,18 @@ class nuclei_dataset(torch.utils.data.Dataset):
         super(nuclei_dataset, self).__init__()
         self.dat = dat
         self.train = train
-        # self.transforms = transforms.Compose([
-                            # transforms.ToPILImage(),
-                            # # transforms.RandomCrop(256),
-                            # transforms.RandomVerticalFlip(),
-                            # transforms.RandomHorizontalFlip()
-                            # ])
+        
     def __getitem__(self, n):
         if not self.train:
             n = self.__len__() - 1 -n
         input, target, dist = self.dat[n]
-        # transforms on input needed
-        # transforms for augmentation needed
+        input = to_brightfield(input)
+        input, target, dist = random_crop_256(input, target, dist, pad_values=[1, 0, 257])
+        input, target, dist = random_flip(input, target, dist)
+        input, target, dist = random_rotate(input, target, dist, pad_values=[1, 0, 257])
+        input = np.expand_dims(input, axis=0)
+        target = np.expand_dims(target, axis=0)
+        dist = np.expand_dims(dist, axis=0)
         
         return input, target, dist
         
@@ -37,11 +38,11 @@ class nuclei_dataset(torch.utils.data.Dataset):
         
 class nuclei_data(object):
     def __init__(self, path):
-        # path is like '../data/stage1_train/[dat.h5]'
+        # path is like '../data/stage1_train/'
         gen_file = not list(Path(path).glob('dat.h5'))
         if gen_file:
             h5f_gen(path)
-            path = path + 'dat.h5'
+        path = path + 'dat.h5'
         self.h5f = h5py.File(path, 'r')
         self.im_ids = list(self.h5f['im_ids'])
     #
@@ -56,46 +57,48 @@ class nuclei_data(object):
     def __len__(self):
         return len(self.im_ids)
     
-    def h5f_gen(path):
-        im_paths = Path(path).glob('*/images/*.png')                
-        h5f = h5py.File(path+'dat.h5', 'w')
-        im_ids = []
-        for im_path in im_paths:
-            im_id = im_path.parts[-3]
-            im_ids.append(im_id)
-            im_path = str(im_path)
-            
-            im = Image.open(im_path).convert('L')
-            h5f.create_dataset(im_id+'.im', data=np.array(im))
-            
-            mask_paths = Path(path+im_id).glob('/masks/*.png')
-            mask, dist = get_mask_and_dist(mask_paths)
-            h5f.create_dataset(im_id+'.mask', data=mask)
-            h5f.create_dataset(im_id+'.dist', data=dist)
-        h5f.create_dataset('im_ids', data=im_ids)
-        
-        h5f.close()    
-        
-                
-    def get_mask_and_dist(mask_paths):
-        mask_paths = list(mask_paths)
-        
-        sample = np.array(Image.open(mask_paths[0]))
-        mask = np.zeros_like(sample)
-        dist = np.full_like(sample, np.inf)
-        
-        for mask_path in mask_paths:
-            mask_ = np.array(Image.open(mask_path))
-            mask_ = np.where(mask_, 1, 0)
-            mask = np.logical_or(mask_, mask)
-            mask = np.where(mask, 1, 0)
-        
-            dist_ = dte(mask_) + dte(1 - mask_)
-            dist = np.dstack((dist, dist_))
-            dist = np.amin(dist, axis=2)
-            
-        return mask, dist
-        
+def h5f_gen(path):
+    im_paths = Path(path).glob('*/images/*.png')                
+    h5f = h5py.File(path+'dat.h5', 'w')
+    im_ids = []
+    for im_path in im_paths:
+        im_id = im_path.parts[-3]
+        im_ids.append(im_id)
+        im_path = str(im_path)
+
+        im = Image.open(im_path).convert('L')
+        im = np.array(im) / 255.
+        h5f.create_dataset(im_id+'.im', data=im)
+
+        mask_paths = map(str, list(Path(path+im_id+'/masks/').glob('*.png')))
+        mask, dist = get_mask_and_dist(mask_paths)
+        h5f.create_dataset(im_id+'.mask', data=mask)
+        h5f.create_dataset(im_id+'.dist', data=dist)
+    im_ids = np.array(im_ids, dtype='object')
+    dt = h5py.special_dtype(vlen=str)
+    h5f.create_dataset('im_ids', data=im_ids, dtype=dt)
+
+    h5f.close()    
+
+def get_mask_and_dist(mask_paths):
+    mask_paths = list(mask_paths)
+
+    sample = np.array(Image.open(mask_paths[0]))
+    mask = np.zeros_like(sample)
+    dist = np.full(sample.shape, np.inf)
+
+    for mask_path in mask_paths:
+        mask_ = np.array(Image.open(mask_path))
+        mask_ = np.where(mask_, 1, 0)
+        mask = np.logical_or(mask_, mask)
+        mask = np.where(mask, 1, 0)
+
+        dist_ = dte(mask_) + dte(1 - mask_)
+        dist = np.dstack((dist, dist_))
+        dist = np.amin(dist, axis=2)
+
+    return mask, dist
+
 
 
 #
--- custom/utils.py
+++ custom/utils.py
@@ -2,8 +2,9 @@ import torch
 import random
 import numpy as np
 from torchvision.transforms import ToPILImage
+from scipy.ndimage.interpolation import rotate
 
-def conditioned_inverse(im):
+def to_brightfield(im):
     im = np.array(im)
     not_brightfield = np.mean(im) < 0.45
     if not_brightfield: im = 1 - im
@@ -15,26 +16,56 @@ def conditioned_inverse(im):
     
     return im
 
-def random_crop(im, output_size=256, pad_value=0):
-    h, w = im.shape
+def random_crop_256(*argv, pad_values=None):
     th, tw = 256, 256
-    if h < th:
-        im = np.pad(im, ((0, th-h), (0, 0)), 'constant', constant_values=(0, pad_value))
-    if w < tw:
-        im = np.pad(im, ((0, 0), (0, tw-w)), 'constant', constant_values=(0, pad_value))
-    h, w = im.shape
-    i = random.randint(0, h - th)
-    j = random.randint(0, w - tw)
-    im = im[i:i+th, j:j+tw]
+    ims = []
+    for idx, im in enumerate(argv):
+        h, w = im.shape
+        pad_value = pad_values[idx]
+        if h < th:
+            im = np.pad(im, ((0, th-h), (0, 0)), 'constant', constant_values=(0, pad_value))
+        if w < tw:
+            im = np.pad(im, ((0, 0), (0, tw-w)), 'constant', constant_values=(0, pad_value))
+        h, w = im.shape
+        if idx==0:
+            i = random.randint(0, h - th)
+            j = random.randint(0, w - tw)
+        im = im[i:i+th, j:j+tw]
+        ims.append(im)
     
-    return im.astype(float)
+    return ims
+
+def random_rotate(*argv, pad_values=None):
+    ims = []
+    for idx, im in enumerate(argv):
+        pad_value = pad_values[idx]
+        if idx==0:
+            angle = random.uniform(0., 90.)
+        im = rotate(im, angle, reshape=False, cval=pad_value)
+        ims.append(im)
+    
+    return ims
+
+def random_flip(*argv):
+    ud, lr = np.random.uniform(size=2) < .5
+    ims = []
+    for im in argv:
+        if ud:
+            im = np.flipud(im)
+        if lr:
+            im = np.fliplr(im)
+        ims.append(im)
+    
+    return ims
     
 def get_weight_c(mask, factor01=(.5, .5)):
+    if not isinstance(mask, np.ndarray):
+        mask = mask.numpy()
     total_count = mask.size
     class1_count = np.count_nonzero(mask)
     class0_count = total_count - class1_count
-    class1_weight = total_count * factor[1] / class1_count
-    class0_weight = total_count * factor[0] / class0_count
+    class1_weight = total_count * factor01[1] / class1_count
+    class0_weight = total_count * factor01[0] / class0_count
     
     weight_c = np.where(mask, class1_weight, class0_weight)
     
