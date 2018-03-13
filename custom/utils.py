ccad23f911ffc6e5304a61205d018a87024428dd
Saliwanof <harrypotter_china@126.com>
2018-03-13 10:50:54 +0800

    utils added

--- custom/utils.py
+++ custom/utils.py
@@ -2,11 +2,12 @@ import torch
 import random
 import numpy as np
 from torchvision.transforms import ToPILImage
-from scipy.ndimage.interpolation import rotate
+from scipy.ndimage import rotate, zoom
+from scipy import ndimage
 
 def to_brightfield(im):
     im = np.array(im)
-    not_brightfield = np.mean(im) < 0.45
+    not_brightfield = np.mean(im) < 0.42
     if not_brightfield: im = 1 - im
     # im = torch.from_numpy(np.expand_dims(im, axis=0))
     imin, imax = np.amin(im), np.amax(im)
@@ -57,16 +58,55 @@ def random_flip(*argv):
         ims.append(im)
     
     return ims
+
+def random_zoom(*argv, pad_values=None):
+    ims = []
+    scales = np.random.uniform(.8, 1.5, 2)
+    for idx, im in enumerate(argv):
+        pad_value = pad_values[idx]
+        im = zoom(im, scales, order=3, cval=pad_value)
+        ims.append(im)
     
+    return ims
+
 def get_weight_c(mask, factor01=(.5, .5)):
     if not isinstance(mask, np.ndarray):
         mask = mask.numpy()
     total_count = mask.size
     class1_count = np.count_nonzero(mask)
     class0_count = total_count - class1_count
-    class1_weight = total_count * factor01[1] / class1_count
-    class0_weight = total_count * factor01[0] / class0_count
+    try:
+        class1_weight = total_count * factor01[1] / class1_count
+        class0_weight = total_count * factor01[0] / class0_count
+    except:
+        class1_weight = 100
+        class0_weight = 100
     
     weight_c = np.where(mask, class1_weight, class0_weight)
     
     return weight_c
+
+def rle_encoding(x):
+    '''
+    x: numpy array of shape (height, width), 1 - mask, 0 - background
+    Returns run length as list
+    '''
+    dots = np.where(x.T.flatten()==1)[0] # .T sets Fortran order down-then-right
+    run_lengths = []
+    prev = -2
+    for b in dots:
+        if (b>prev+1): run_lengths.extend((b+1, 0))
+        run_lengths[-1] += 1
+        prev = b
+        
+    return ' '.join([str(i) for i in run_lengths])
+
+def mask_seperation(mask):
+    mask = 1 - mask
+    labels, nlabels = ndimage.label(mask)
+    label_arrays = []
+    for label_num in range(1, nlabels+1):
+        label_mask = np.where(labels == label_num, 1, 0)
+        label_arrays.append(label_mask)
+    
+    return label_arrays
